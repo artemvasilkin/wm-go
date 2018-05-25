52398bbde01a862278b3f3213084f6455ffee9a3
artemvasilkin <artemvasilkin14@gmail.com>
2018-05-25 13:36:10 +0300

    new globals

--- config/globals.js
+++ config/globals.js
@@ -1,13 +1,45 @@
 const appRoot = require('app-root-path')
 
+const devServer = 'dev'
+const stageServer = 'stage'
+const prodServer = 'prod'
+
+const servers = [`${devServer}`, `${stageServer}`, `${prodServer}`]
+
+const devDomain = 'io'
+const stageDomain = 'co'
+const prodDomain = 'com'
+
+const domains = [`${devDomain}`, `${stageDomain}`, `${prodDomain}`]
+
+const baseFile = `block.https.weblium.`
+const devFile = `${baseFile}${devDomain}`
+const stageFile = `${baseFile}${stageDomain}`
+const prodFile = `${baseFile}${prodDomain}`
+
+const history =
+  'https://raw.githubusercontent.com/webliumteam/blocks/history/historia_vitae_magistra.json'
+
+const localhost = 'http://localhost:9999/'
+
+const report = `${appRoot.path}/logs/report.txt`.replace('/scripts/', '/')
+const wmConfig = `${appRoot.path}/config/wm-config.json`.replace(
+  '/scripts/',
+  '/'
+)
+
 module.exports = {
-  devFile: 'block.https.weblium.io',
-  stageFile: 'block.https.weblium.co',
-  prodFile: 'block.https.weblium.com',
-  baseFile: 'block.https.weblium.',
-  history:
-    'https://raw.githubusercontent.com/webliumteam/blocks/history/historia_vitae_magistra.json',
-  localhost: 'http://localhost:9999/',
-  report: `${appRoot.path}/logs/report.txt`.replace('/scripts/', '/'),
-  wmConfig: `${appRoot.path}/config/wm-config.json`.replace('/scripts/', '/')
+  servers: servers,
+  devServer: devServer,
+  stageServer: stageServer,
+  prodServer: prodServer,
+  domains: domains,
+  baseFile: baseFile,
+  devFile: devFile,
+  stageFile: stageFile,
+  prodFile: prodFile,
+  history: history,
+  localhost: localhost,
+  report: report,
+  wmConfig: wmConfig
 }
