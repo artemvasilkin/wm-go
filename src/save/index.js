5aca0ebf1a0d9538be8f15a95fbd8b47dc5886a5
artemvasilkin <artemvasilkin14@gmail.com>
2018-07-03 13:30:41 +0300

    improve security

--- src/save/index.js
+++ src/save/index.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 const { gratz, error } = require('../utils/out')
 const { show } = require('../utils/show')
 const { getCurrentBranch } = require('../utils/getCurrentBranch')
@@ -5,21 +7,29 @@ const { getCurrentBranch } = require('../utils/getCurrentBranch')
 const save = message => {
   try {
     if (message && typeof message === 'string') {
-      const currentBranch = getCurrentBranch()
+      if (
+        !fs.existsSync('undefined') &&
+        !fs.existsSync('wm-config.json') &&
+        !fs.existsSync('wm-config')
+      ) {
+        const currentBranch = getCurrentBranch()
 
-      show(`git add .`)
-      show(`git commit -m "${message}"`)
+        show(`git add .`)
+        show(`git commit -m "${message}"`)
 
-      try {
-        show(`git push`)
-        gratz(`pushed to ${currentBranch}`)
-      } catch (error) {
-        if (error.toString().includes('has no upstream branch')) {
-          show(`git push --set-upstream origin ${currentBranch}`)
-          gratz(`new remote branch ${currentBranch} created`)
-        } else {
-          error(error)
+        try {
+          show(`git push`)
+          gratz(`pushed to ${currentBranch}`)
+        } catch (error) {
+          if (error.toString().includes('has no upstream branch')) {
+            show(`git push --set-upstream origin ${currentBranch}`)
+            gratz(`new remote branch ${currentBranch} created`)
+          } else {
+            error(error)
+          }
         }
+      } else {
+        error('ERROR! REMOVE DANGEROOUS FILES!')
       }
     } else {
       error('please, specify commit message')
