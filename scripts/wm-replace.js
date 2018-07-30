7c75d1f4fe63fdc48442cce7cf9a70fcb586ef4e
artemvasilkin <artemvasilkin14@gmail.com>
2018-07-30 12:15:36 +0300

    update replcaer

--- scripts/wm-replace.js
+++ scripts/wm-replace.js
@@ -2,13 +2,6 @@
 
 console.time('wm-replace')
 
-const program = require('commander')
-
-const { kill } = require('../src/kill')
-const { init } = require('../src/init')
-const { republish } = require('../src/republish')
-const { save } = require('../src/save')
-
 const fs = require('fs')
 const { gratz, error } = require('../src/utils/out')
 
@@ -27,16 +20,8 @@ const replacment = [
 ]
 
 if (fs.existsSync(component)) {
-  let shouldRepublish = false
-
-  gratz('exists')
-
   for (var i = 0; i < replacings.length; i++) {
-    gratz('for loop started')
-
     while (replacings[i].test(fs.readFileSync(component).toString())) {
-      gratz('while loop started')
-
       fs.writeFileSync(
         component,
         fs
@@ -44,24 +29,6 @@ if (fs.existsSync(component)) {
           .toString()
           .replace(replacings[i], replacment[i])
       )
-
-      shouldRepublish = true
-    }
-  }
-
-  if (shouldRepublish) {
-    save('fix sticky header')
-
-    if (program.kill && program.kill.length > 0) {
-      kill(program.kill, 'y')
-    }
-
-    if (program.init && program.init.length > 0) {
-      init(program.init)
-    }
-
-    if (program.republish && program.republish.length > 0) {
-      republish(program.republish)
     }
   }
 } else {
