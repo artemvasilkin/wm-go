206d6add39f0291511b43db39afc2809f10a0f46
artemvasilkin <artemvasilkin14@gmail.com>
2018-08-01 16:33:52 +0300

    add wm-replace

--- scripts/wm-go.js
+++ scripts/wm-go.js
@@ -8,6 +8,8 @@ const { figlet } = require('../src/utils/figlet')
 const { getBranches } = require('../src/utils/getBranches')
 
 const { goRound: go } = require('../src/goRound')
+const { replace } = require('../src/replace')
+const { file, original, replacement } = require('../config/replace')
 const { kill } = require('../src/kill')
 const { init } = require('../src/init')
 const { republish } = require('../src/republish')
@@ -17,12 +19,18 @@ const [, , ...args] = process.argv
 const pattern = args.length ? args[0] : false
 
 program
+  .option('-k, --kill [server]', 'kill', '')
+  .option('-r, --replace', 'replace', '')
   .option('-i, --init [server]', 'init', '')
   .option('-r, --republish [server]', 'republish', '')
-  .option('-k, --kill [server]', 'kill', '')
   .parse(process.argv)
 
-const branches = getBranches(pattern).filter(branch => branch.match('/dev'))
+// const branches = getBranches(pattern).filter(branch => branch.match('/dev'))
+const branches = [
+  'w/zapdos/about/dev',
+  'w/series-4/careers/dev',
+  'w/zapdos/cover/dev'
+]
 const branchesLength = branches.length
 
 figlet(`${branchesLength} branches found`)
@@ -33,6 +41,10 @@ go(branches, (branch, index) => {
 
   if (program.kill && program.kill.length > 0) {
     kill(program.kill, 'y')
+  }
+
+  if (program.replace) {
+    replace(file, original, replacement, branch)
   }
 
   if (program.init && program.init.length > 0) {
--- src/replace/index.js
+++ src/replace/index.js
@@ -1,8 +1,15 @@
 const fs = require('fs')
+const appRoot = require('app-root-path')
 
+const { save } = require('../save')
 const { error } = require('../utils/out')
 
-const replace = (file, original, replacement) => {
+const failedList = `${appRoot.path}/logs/failed-to-replace.txt`.replace(
+  '/scripts/',
+  '/'
+)
+
+const replace = (file, original, replacement, branch) => {
   if (fs.existsSync(file)) {
     for (var i = 0; i < original.length; i++) {
       if (original[i].test(fs.readFileSync(file).toString())) {
@@ -13,13 +20,20 @@ const replace = (file, original, replacement) => {
             .toString()
             .replace(original[i], replacement[i])
         )
+
+        save('auto replace')
+      } else {
+        if (branch) {
+          fs.appendFileSync(failedList, `${branch}\n`)
+        }
       }
     }
   } else {
-    error('no component')
+    error('no file')
   }
 }
 
 module.exports = {
-  replace: (file, original, replacement) => replace(file, original, replacement)
+  replace: (file, original, replacement, branch) =>
+    replace(file, original, replacement, branch)
 }
